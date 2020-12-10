c73d58f9d10de4d6847678fcfcf7a1b2263b8aed
sternrc <sternrc@dukes.jmu.edu>
2020-12-10 18:12:25 -0500

    sample note format

--- src/App.js
+++ src/App.js
@@ -14,20 +14,6 @@ function App() {
   dispatch(loadDay(4, 7));
   }, [dispatch]);
  
-/*   const notes = [
-    {id: 1, number: 1, message: "hey"},
-    {id: 2, number: 2, message: "hi"},
-    {id: 3, number: 3, message: "hello"},
-  ];
- */
-  //const notes = useSelector(state => state.memories);
-  //const dispatch = useDispatch();
-
-  //const onLoad = () => {
-  //  dispatch(loadNotes([
-  //    {number: 1, message: "random message"},
-  //   ]));
-  //}
   return (
      <div className="App">
       <header className="App-header">   
@@ -39,15 +25,23 @@ function App() {
       <h3>(google maps link)</h3>
       <h2>Dates</h2>
       <h3>(calendar)</h3>
-        <div className="notes">
+        <div>
           <h2>Notes</h2>
-            <div>
-              {notes.map(note => <Note key={note.id} note={note}/>)}
-            </div>
-            {/* <button onCLick={onLoad}>test button</button>
-            {notes.map(note => <Notes note={note} />)} */}
-            
-          </div>
+          <div className="notes">
+            <li>
+              1. sample note
+              {/* use this when backend stuff works {notes.map(note => <Note key={note.id} note={note}/>)} */}
+            </li>
+            <li>
+              2. sample note
+              {/* use this when backend stuff works {notes.map(note => <Note key={note.id} note={note}/>)} */}
+            </li>
+            <li>
+              3. sample note
+              {/* use this when backend stuff works {notes.map(note => <Note key={note.id} note={note}/>)} */}
+            </li>
+          </div>            
+        </div>
       </div>
   );
 }
