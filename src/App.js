71f20f5316152368b6272428700ead06969c1910
sternrc <sternrc@dukes.jmu.edu>
2020-12-13 16:54:10 -0500

    dest funct is commented becuase theres null error

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import React, {useEffect} from 'react';
 
 const id = 0;
 
-/* function eventHandler(event) {
+/* function destFunction() {
+  var d = document.getElementById("dest").value;
+  document.getElementById("loc").innerHTML = d;
 
-}
- */
-// function text() {
-//   const t = document.getElementById("")
-// }
+} */
 
 function App() {
 
@@ -26,8 +24,7 @@ function App() {
   const onAdd = () => {
     dispatch(startAddingNote(id));
   }
-  
- 
+   
   return (
      <div className="App">
       <header className="App-header">   
@@ -37,36 +34,22 @@ function App() {
       </header>
       <h2>Destination</h2>
       <input id="dest"></input>
-      <button id="d">go!</button>
-      
-      {/* var text = document.getElementById('dest').value;
-      <p id="addy">text</p> */}
+      {/* <button onClick={destFunction()} id="d">go!</button>  */}
+      <button id="d">go!</button> 
 
-      {/* function eventHandler() { 
-            var x = document.getElementById('textArea'); 
-            x.value = "GeeksforGeeks"; 
-        } 
-       */}
-      
-            {/* var loc = document.getElementById("dest").value;
-          document.getElementById("addy").innerHTML = loc}; */}
-        
-
+      <p id="loc"></p>
 
       <h2>Dates</h2>
       <label>Departure Date: </label>
       <input type="date" id="departure"></input>
       <label>Arrival Date: </label>
       <input type="date" id="arrival"></input>
-      
 
         <div>
           <h2>Notes</h2>
           <div className="notes">
           <button id="new" onClick={onAdd}>new note</button>
-            {/* <li> */}
               {notes.map(note => <Note key={note.id} note={note}/>)}
-            {/* </li> */}
           </div>            
         </div>
       </div>
